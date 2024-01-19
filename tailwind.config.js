f0326a881bdb165d5bf5480502a46155f9083651
Hmazah <alhamza2012@gmail.com>
2024-01-19 10:00:00 +0200

    add  edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',     // صفحات Next.js
-    './components/**/*.{js,ts,jsx,tsx}', // مكوناتك
-    './app/**/*.{js,ts,jsx,tsx}',        // إذا كنت تستخدم App Router
+    './src/app/**/*.{js,ts,jsx,tsx}',     // صفحات Next.js
+    './src/components/**/*.{js,ts,jsx,tsx}', // مكوناتك
+    './src/app/**/*.{js,ts,jsx,tsx}',        // إذا كنت تستخدم App Router
   ],
   theme: {
     extend: {},
